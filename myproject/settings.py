fc2596cfb88f007c96ee8d80160709cae9ec41bf
abdussalam02 <samaniabdussalam02@gmail.com>
2023-03-21 19:24:52 +0530

    testing 12

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-d702.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-d702.up.railway.app/']
+ALLOWED_HOSTS = ['burak.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://burak.up.railway.app/']
 
 # Application definition
 
